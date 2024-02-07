c24296fd16ee5e4e9d5e856329a1962d9fe611d6
guillermobermejo <90947061+guillermobermejo@users.noreply.github.com>
2024-02-07 13:05:05 -0500

    Project Header Test

--- assets/js/main.js
+++ assets/js/main.js
@@ -114,6 +114,24 @@
 
 		// Section: Project-info
 
+		$window.on('load', function() {
+
+			$('#gerrymandering-project-header-table').poptrox({
+				caption: function($a) { return $a.next('h3').text(); },
+				overlayColor: '#2c2c2c',
+				overlayOpacity: 0.85,
+				popupCloserText: '',
+				popupLoaderText: '',
+				selector: '.work-item a.image',
+				usePopupCaption: true,
+				usePopupDefaultStyling: false,
+				usePopupEasyClose: false,
+				usePopupNav: true,
+				windowMargin: (breakpoints.active('<=small') ? 0 : 50)
+			});
+
+		});		
+
 			$window.on('load', function() {
 
 				$('#section-gerrymandering-project-info').poptrox({
